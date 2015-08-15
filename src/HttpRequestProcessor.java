c2942047ddbec926fa80969adc44840c1b72f579
muzahidengr <muzahid.engr@gmail.com>
2015-08-16 00:09:19 +0600

    Thread implementation of HTTP request processor class

--- src/HttpRequestProcessor.java
+++ src/HttpRequestProcessor.java
@@ -4,14 +4,32 @@
 * Environment:       JDK 1.6
 * Date:              15-AUG-2015
 * */
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Map;
+import java.util.TreeMap;
 
-/**A class to process HTTP request*/
-public class HttpRequestProcessor {
+/**A thread class to process HTTP request*/
+public class HttpRequestProcessor implements Runnable {
+	java.net.Socket clientSocket = null;
+
+	public HttpRequestProcessor(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	/**process client socket request as HTTP request*/
+	public void process() {
+		//TODO
+	}
+
+	@Override
+	public void run() {
+		process();
+	}
 
 	/**create new thread for each socket request*/
 	public static void processClient(Socket clientSocket) {
-		//TODO
+		new Thread(new HttpRequestProcessor(clientSocket)).start();
 	}
 
 }
