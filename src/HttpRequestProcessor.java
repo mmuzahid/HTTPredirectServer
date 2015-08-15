636382646501acbc3cc41a559aea037ac53be052
muzahidengr <muzahid.engr@gmail.com>
2015-08-16 00:13:16 +0600

    added implementation of http request process method

--- src/HttpRequestProcessor.java
+++ src/HttpRequestProcessor.java
@@ -18,13 +18,48 @@ public class HttpRequestProcessor implements Runnable {
 	}
 
 	/**process client socket request as HTTP request*/
-	public void process() {
-		//TODO
+	public void process() throws IOException, InterruptedException {
+		StringBuffer requestBody = new StringBuffer("");
+		java.io.InputStream input = clientSocket.getInputStream();
+		java.io.InputStreamReader reader = new java.io.InputStreamReader(input);
+		do {
+			requestBody.append((char) input.read());
+		} while (input.available() > 0);
+		
+		String reqMessage = requestBody.toString();
+		String lang = "en";
+
+		String noCacheHeader = "Cache-Control: no-cache, no-store, must-revalidate\r\n" + // HTTP 1.1 client
+				"Pragma: no-cache\r\n" + // HTTP 1.0, for client
+				"Expires: 0\r\n"; // HTTP 1.0, for client and proxies
+
+		String redirectLocation = "https://" + lang + ".wikipedia.org/wiki/URL_redirection";	
+		String responseBody = new String("HTTP/1.1 301 Moved Permanently\r\n"
+				+ // 200 OK Moved Permanently\r\n
+				"Location: " + redirectLocation + "\r\n"
+				+ "Content-Type: text/html; charset=utf-8\r\n" + noCacheHeader
+				+ "\r\n server time: " + (new java.util.Date())
+				+ "<br \\> your request: <hr \\>"
+				+ reqMessage.replace("\n", "<br \\>") + "\r\n");
+
+		//System.out.println(requestBody.toString());
+		java.io.OutputStream out = clientSocket.getOutputStream();
+		out.write(responseBody.getBytes("UTF-8"));
+		out.flush();
+		out.close();
+		reader.close();
+		clientSocket.close();
 	}
 
 	@Override
 	public void run() {
-		process();
+		try {
+			process();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}	
 	}
 
 	/**create new thread for each socket request*/
